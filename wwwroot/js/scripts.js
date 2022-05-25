81054e1ed5ff3c7005fc28d6d1ad5cbd4246308a
AndreiShtef <shtefanai74@gmail.com>
2022-05-25 23:16:24 +0300

    Дополнения script, index

--- wwwroot/js/scripts.js
+++ wwwroot/js/scripts.js
@@ -1,1 +1,47 @@
-﻿
+﻿/*jshint esversion: 6 */
+const uri = "/api/manufacturers/";
+let items = null;
+
+function loadManufs() {
+    var i, j, x = "";
+    var request = new XMLHttpRequest();
+    request.open("GET", uri, false);
+    request.onload = function () {
+        items = JSON.parse(request.responseText);
+        for (i in items) {
+            x += "<hr>";
+            x += "<h4> Производитель: " + items[i].Name + " : <a href='" + items[i].Name + "'>" + items[i].Name + "</a></h4>";
+            x += "<button type='button' class='btn btn-sm btn-outline-secondary' onclick='DeleteManufacturers(" + items[i].ManufId + ");'>Удалить</button>";
+
+            for (j in items[i].cars) {
+                x += "<div class='col-10'>";
+                x += "<h4>" + items[i].cars[j].ModelName + "</h4>";
+                x += "<p>" + items[i].cars[j].YearOfProd + "</p><br>";
+                x += "</div>";
+            }
+        }
+        document.getElementById("manufsDiv").innerHTML = x;
+    };
+    request.send();
+}
+
+function deleteManufs(id) {
+    var request = new XMLHttpRequest();
+    var url = uri + id;
+    request.open("DELETE", url, false);
+    request.onload = function () {
+        loadManufs();
+    };
+    request.send();
+}
+
+function createManufs() {
+    var nameText = document.getElementById("createDiv").value;
+    var request = new XMLHttpRequest();
+    request.open("POST", uri);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.onload = function () {
+        loadManufs();
+    };
+    request.send(JSON.stringify({ name: nameText }));
+}
